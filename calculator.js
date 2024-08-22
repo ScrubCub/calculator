750de6aa427b8beb7eef0e7bcd9d192000315d79
Kyle Bryan Cagasan <cagasankylebryan@gmail.com>
2024-08-22 13:21:47 +0800

    Add event listeners to buttons
    Event listeners for buttons are added with output
    to screen

--- calculator.js
+++ calculator.js
@@ -1,20 +1,22 @@
 let currentOperations = [];
 let currentNumbers = [];
+let numberEntered = '';
+let recentlyEvaled = false;
 
 let operations = {
-    add: function(firstNumber, secondNumber) {
+    '+': function(firstNumber, secondNumber) {
         return firstNumber + secondNumber;
     },
-    subtract: function(firstNumber, secondNumber) {
+    '-': function(firstNumber, secondNumber) {
         return firstNumber - secondNumber;
     },
-    multiply: function(firstNumber, secondNumber) {
+    'x': function(firstNumber, secondNumber) {
         return firstNumber * secondNumber;
     },
-    divide: function(firstNumber, secondNumber) {
+    '/': function(firstNumber, secondNumber) {
         return firstNumber / secondNumber;
     },
-    raise: function(firstNumber, secondNumber) {
+    '^': function(firstNumber, secondNumber) {
         return firstNumber ** secondNumber;
     }
 }
@@ -28,7 +30,7 @@ function evaluation(finalNumbers, finalOperations) {
         let currentOperation = finalOperations[currentIndex];
         let nextOperation = finalOperations[nextIndex];
 
-        if (nextOperation == "multiply" || nextOperation == "divide") {
+        if (nextOperation == "x" || nextOperation == "/") {
 
             let result = operations[nextOperation](finalNumbers[nextIndex], finalNumbers[nextIndex + 1]);
             finalNumbers.splice(nextIndex, 2, result);
@@ -82,10 +84,69 @@ entireContainer['rowOne'].setAttribute('style', 'display: flex; flow-direction: 
 
 clearButton.setAttribute('style', 'background-color: gray; border: solid red 2px; flex: 1')
 clearButton.textContent = 'AC'
+clearButton.addEventListener('click', function() {
+    if (recentlyEvaled) {
+        entireContainer['screen'].textContent = "";
+        recentlyEvaled = false;
+    }
+    numberEntered = '';
+    currentNumbers = [];
+    currentOperations = [];
+    entireContainer['screen'].textContent = '';
+})
+
+
 backspaceButton.setAttribute('style', 'background-color: gray; border: solid red 2px; flex: 2');
 backspaceButton.textContent = '<-'
+backspaceButton.addEventListener('click', function() {
+    if (recentlyEvaled) {
+        entireContainer['screen'].textContent = "";
+        recentlyEvaled = false;
+    }
+
+    if (entireContainer['screen'].textContent.at(-1).endsWith(" ")) {
+        let tempText = entireContainer['screen'].textContent;
+        tempText = tempText.slice(0,-3);
+        entireContainer['screen'].textContent = tempText;
+        currentOperations.pop();
+        numberEntered = `${currentNumbers.pop()}`
+
+    } else {
+        let tempText = entireContainer['screen'].textContent;
+        tempText = tempText.slice(0, -1);
+        entireContainer['screen'].textContent = tempText;
+        numberEntered = numberEntered.slice(0, -1);
+    }
+    
+    
+})
+
+
+
 divideButton.setAttribute('style', 'background-color: gray; border: solid red 2px; flex: 1');
 divideButton.textContent = operationSymbols.shift();
+divideButton.addEventListener('click', function() {
+    if (recentlyEvaled) {
+        entireContainer['screen'].textContent = "";
+        recentlyEvaled = false;
+    }
+    if (entireContainer['screen'].textContent.at(-1).endsWith(" ")) {
+        let tempText = entireContainer['screen'].textContent;
+        tempText = tempText.slice(0,-3);
+        tempText += ` ${divideButton.textContent} `
+        entireContainer['screen'].textContent = tempText;
+        currentOperations.pop();
+        currentOperations.push(divideButton.textContent);
+        
+    } else {
+        entireContainer['screen'].textContent += ` ${divideButton.textContent} `
+        currentOperations.push(divideButton.textContent);
+        currentNumbers.push(Number(numberEntered));
+        numberEntered = '';
+
+    }
+})
+
 
 entireContainer['rowOne'].appendChild(clearButton);
 entireContainer['rowOne'].appendChild(backspaceButton);
@@ -102,12 +163,42 @@ for (let i = 3; i < 6; i++) {
         let newDiv = createDiv();
         newDiv.setAttribute('style', 'background-color: gray; border: solid red 2px; flex: 1');
         newDiv.textContent = `${j}`
+        newDiv.addEventListener('click', function() {
+            if (recentlyEvaled) {
+                entireContainer['screen'].textContent = '';
+                recentlyEvaled = false;
+            }
+            numberEntered += `${j}`
+            entireContainer['screen'].textContent += `${j}`;
+            ;
+        })
         entireContainer[key].appendChild(newDiv);
         
         if (j % 3 == 0) {
             newDiv = createDiv();
             newDiv.setAttribute('style', 'background-color: gray; border: solid red 2px; flex: 1');
-            newDiv.textContent = `${operationSymbols.shift()}`
+            newDiv.textContent = `${operationSymbols.shift()}`;
+            newDiv.addEventListener('click', function() {
+                if (recentlyEvaled) {
+                    entireContainer['screen'].textContent = "";
+                    recentlyEvaled = false;
+                }
+                if (entireContainer['screen'].textContent.at(-1).endsWith(" ")) {
+                    let tempText = entireContainer['screen'].textContent;
+                    tempText = tempText.slice(0,-3);
+                    tempText += ` ${newDiv.textContent} `
+                    entireContainer['screen'].textContent = tempText;
+                    currentOperations.pop();
+                    currentOperations.push(newDiv.textContent);
+                    
+                } else {
+                    entireContainer['screen'].textContent += ` ${newDiv.textContent} `
+                    currentOperations.push(newDiv.textContent);
+                    currentNumbers.push(Number(numberEntered));
+                    numberEntered = '';
+
+                }
+            })
             entireContainer[key].appendChild(newDiv);
 
         }
@@ -127,6 +218,38 @@ enterButton.setAttribute('style', 'background-color: gray; border: solid red 2px
 decimalButton.textContent = '.';
 zeroButton.textContent = '0';
 enterButton.textContent = '=';
+
+decimalButton.addEventListener('click', function() {
+    if (recentlyEvaled) {
+        entireContainer['screen'].textContent = "";
+        recentlyEvaled = false;
+    }
+    if (numberEntered.indexOf('.')== -1 && numberEntered != '') {
+        numberEntered += '.';
+        entireContainer['screen'].textContent += '.';
+    }
+})
+
+zeroButton.addEventListener('click', function() {
+    if (recentlyEvaled) {
+        entireContainer['screen'].textContent = "";
+        recentlyEvaled = false;
+    }
+    numberEntered += '0'
+    entireContainer['screen'].textContent += `0`;
+})
+
+enterButton.addEventListener('click', function() {
+    if (!entireContainer['screen'].textContent.at(-1).endsWith(" ")) {
+        currentNumbers.push(Number(numberEntered));
+        entireContainer['screen'].textContent = `${evaluation(currentNumbers, currentOperations)}`;
+        numberEntered = '';
+        currentNumbers = [];
+        currentOperations = [];
+        recentlyEvaled = true;
+
+    }
+})
 
 entireContainer['rowFive'].setAttribute('style', 'display: flex; flow-direction: row; flex: 1');
 entireContainer['rowFive'].appendChild(decimalButton);
